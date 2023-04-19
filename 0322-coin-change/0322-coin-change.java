fe11ba60f35e07e4a4957f47a1dbb8e2f9df6807
Yong su Kwon <82052272+DevFrog92@users.noreply.github.com>
2023-04-19 21:38:15 +0900

    Time: 15 ms (74.65%), Space: 41.7 MB (97.88%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,18 +1,16 @@
+import java.util.Arrays;
 class Solution {
-    int[] dp = new int[10001];
     public int coinChange(int[] coins, int amount) {
-        if(amount > 0 && dp[amount] != 0) return dp[amount];
-        if(amount == 0) return 0;
-        if(amount < 0) return -1;
-
-        int result = Integer.MAX_VALUE;
-        for(int coin: coins) {
-            int tmp = coinChange(coins, amount - coin);
-            if(tmp == -1) continue;
-            result = Math.min(result, tmp + 1);
+        int[] dp = new int[amount+1];
+        Arrays.fill(dp,  amount+1);
+        dp[0] = 0;
+        for (int i = 0; i <= amount ; i++) {
+            for(int coin: coins) {
+                if(i + (long)coin > (long)amount) continue;
+                dp[i + coin] = Math.min(dp[i + coin], dp[i] + 1);
+            }
         }
 
-        dp[amount] = result == Integer.MAX_VALUE ? -1 : result;
-        return dp[amount];
+        return dp[amount] == amount + 1 ? -1 : dp[amount];
     }
 }
