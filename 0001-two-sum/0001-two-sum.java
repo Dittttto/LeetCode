b791f901128869e606200a9ab1867cb8113a307c
Yong su Kwon <82052272+DevFrog92@users.noreply.github.com>
2023-04-28 18:45:40 +0900

    Time: 63 ms (38.48%), Space: 43.1 MB (17.94%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,13 +1,16 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
+        int[] answer = new int[2];
         for(int i = 0; i< nums.length; i++) {
             for(int j = i+1;j< nums.length;j++) {
                 if(nums[i] + nums[j] == target){
-                    return new int[]{i,j};
+                    answer[0] = i;
+                    answer[1] = j;
+                    break;
                 }
             }
         }
         
-        return new int[]{}; 
+        return answer; 
     }
 }
