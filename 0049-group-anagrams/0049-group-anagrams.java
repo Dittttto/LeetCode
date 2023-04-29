89ff5546c050af6243118689bf090790f800c213
Yong su Kwon <82052272+DevFrog92@users.noreply.github.com>
2023-04-30 01:41:17 +0900

    Time: 776 ms (5.00%), Space: 45.9 MB (51.58%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -0,0 +1,50 @@
+import java.util.*;
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        boolean[] check = new boolean[strs.length];
+        List<List<String>> list = new ArrayList<>();
+        
+        for(int i = 0; i< strs.length; i++) {
+            if(check[i]) continue;
+            List<String> tmp = new ArrayList<>();
+            tmp.add(strs[i]);
+            check[i] = true;
+            
+            for(int j = i+1; j<strs.length;j++) {
+                if(strs[i].length() != strs[j].length()) continue;
+                if(strs[i].equals(strs[j])){
+                    tmp.add(strs[j]);
+                    check[j] = true;
+                }else {
+                    if(checkAnagram(strs[i], strs[j])) {
+                        tmp.add(strs[j]);
+                        check[j] = true;
+                    }
+                }
+            }
+            
+            Collections.sort(tmp);
+            list.add(tmp);
+        }
+        
+        return list;
+    }
+    
+    public boolean checkAnagram(String str1, String str2) {
+        
+        int[] alph1 = new int[26];
+        int[] alph2 = new int[26];
+        char[] arr = str1.toCharArray();
+        char[] arr2 = str2.toCharArray();
+        for(int i = 0; i < arr.length; i++) {
+            alph1[arr[i] - 'a' ] += 1;
+            alph2[arr2[i] - 'a' ] += 1;
+        }
+        
+        for(int i=0; i< alph1.length; i++) {
+            if(alph1[i] != alph2[i]) return false;
+        }
+        
+        return true;
+    }
+}
