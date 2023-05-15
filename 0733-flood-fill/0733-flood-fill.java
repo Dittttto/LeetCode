930ef849509c4e60eb253557322db8c95b01beee
Yong su Kwon <82052272+DevFrog92@users.noreply.github.com>
2023-05-16 01:13:34 +0900

    Time: 2 ms (11.62%), Space: 43.4 MB (58.53%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -3,10 +3,12 @@ class Solution {
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
         boolean[][] check = new boolean[image.length][image[0].length];
         int origin = image[sr][sc];
+        
         Queue<int[]> q = new LinkedList<>();
         q.add(new int[]{sr, sc});
         image[sr][sc] = color;
         check[sr][sc] = true;
+        
         int[] dx = new int[]{-1,0,1,0};
         int[] dy = new int[]{0,-1,0,1};
         while(!q.isEmpty()) {
@@ -18,8 +20,7 @@ class Solution {
                 
                 if(x < 0 || x >= image.length) continue;
                 if(y < 0 || y >= image[0].length) continue;
-                if(image[x][y] != origin) continue;
-                if(check[x][y]) continue;
+                if(check[x][y] || image[x][y] != origin) continue;
                 q.add(new int[]{x,y});
                 image[x][y] = color;
                 check[x][y] = true;
