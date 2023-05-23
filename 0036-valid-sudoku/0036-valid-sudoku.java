35ec264fd0a4f0e71b82dc9bfc299a37ea45ba04
Yong su Kwon <82052272+DevFrog92@users.noreply.github.com>
2023-05-24 01:11:11 +0900

    Time: 4 ms (49.42%), Space: 44 MB (6.58%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -1,9 +1,11 @@
 class Solution {
+    static char[][] map;
     public boolean isValidSudoku(char[][] board) {
+        map = board;
         for(int i = 0; i< board.length ;i ++){
             for(int j = 0; j < board.length; j++) {
-                if(!checkRow(i, board) || !checkCol(j, board)) return false;
-                if(i % 3 == 0 && j %3 == 0 && !checkRec(i, j, board)) return false;
+                if(!checkRow(i) || !checkCol(j)) return false;
+                if(i % 3 == 0 && j %3 == 0 && !checkRec(i, j)) return false;
             }
         }
         return true;
@@ -14,9 +16,9 @@ class Solution {
         return true;
     }
     
-    public boolean checkRow(int x, char[][] board) {
+    public boolean checkRow(int x) {
         int[] check = new int[10];
-        for(char c: board[x]) {
+        for(char c: map[x]) {
             if(c == '.') continue;
             check[c - '0'] += 1;
         }
@@ -24,23 +26,22 @@ class Solution {
         return count(check);
     }
     
-    public boolean checkCol(int y, char[][] board) {
+    public boolean checkCol(int y) {
         int[] check = new int[10];
-        for(int i = 0; i< board.length; i++) {
-            char c = board[i][y];
-            if(c == '.') continue;
-            check[c - '0'] += 1;
+        for(int i = 0; i< map.length; i++) {
+            if(map[i][y] == '.') continue;
+            check[map[i][y] - '0'] += 1;
         }
         
         return count(check);
     }
     
-    public boolean checkRec(int x, int y, char[][] board) {
+    public boolean checkRec(int x, int y) {
         int[] check = new int[10];
         for(int i = x; i < x+3 ;i++) {
             for(int j = y; j < y+3; j++) {
-                if(board[i][j] == '.') continue;
-                check[board[i][j] - '0'] += 1;
+                if(map[i][j] == '.') continue;
+                check[map[i][j] - '0'] += 1;
             }
         }
         
