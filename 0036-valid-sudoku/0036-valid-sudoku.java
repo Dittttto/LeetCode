4f2198de9c5c53efbee480b49612af84fe58fd2a
Yong su Kwon <82052272+DevFrog92@users.noreply.github.com>
2023-05-24 01:06:42 +0900

    Time: 3 ms (69.35%), Space: 43.1 MB (33.80%) - LeetHub

---
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -0,0 +1,48 @@
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        for(int i = 0; i< board.length ;i ++){
+            for(int j = 0; j < board.length; j++) {
+                if(!checkRow(i, board) || !checkCol(j, board)) return false;
+                if(i % 3 == 0 && j %3 == 0 && !checkRec(i, j, board)) return false;
+            }
+        }
+        return true;
+    }
+    
+    public boolean checkRow(int x, char[][] board) {
+        int[] check = new int[10];
+        for(char c: board[x]) {
+            if(c == '.') continue;
+            check[c - '0'] += 1;
+        }
+        
+        for(int ch: check) if(ch > 1) return false;
+        return true;
+    }
+    
+    public boolean checkCol(int y, char[][] board) {
+        int[] check = new int[10];
+        for(int i = 0; i< board.length; i++) {
+            char c = board[i][y];
+            if(c == '.') continue;
+            check[c - '0'] += 1;
+        }
+        
+        for(int ch: check) if(ch > 1) return false;
+        return true;
+    }
+    
+    public boolean checkRec(int x, int y, char[][] board) {
+        int[] check = new int[10];
+        
+        for(int i = x; i < x+3 ;i++) {
+            for(int j = y; j < y+3; j++) {
+                if(board[i][j] == '.') continue;
+                check[board[i][j] - '0'] += 1;
+            }
+        }
+        
+        for(int ch: check) if(ch > 1) return false;
+        return true;
+    }
+}
